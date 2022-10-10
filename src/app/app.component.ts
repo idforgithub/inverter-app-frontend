08977f53ab30445e5a0168a66bbc564b6f8df04f
Rendy Sulu <idforgithub@users.noreply.github.com>
2022-10-10 20:05:03 +0700

    second commmit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import * as ApexCharts from 'apexcharts';
+import { Inverter, InverterData } from './inverter.model';
+import { InverterService } from './inverter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'inverter-app-frontend';
+
+export class AppComponent implements OnInit {
+  public inverters?: Inverter[];
+
+  constructor(private inverterService: InverterService){}
+
+  ngOnInit(): void {
+    this.getInverters();
+  }
+
+  private renderChart(data: Inverter[]): void{
+    var options = {
+      chart: {
+        height: 280,
+        type: "area"
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'smooth',
+      },
+      series: data,
+      fill: {
+        type: "gradient",
+        gradient: {
+          shadeIntensity: 1,
+          opacityFrom: 0.7,
+          opacityTo: 0.9,
+          stops: [0, 90, 100]
+        }
+      },
+      xaxis: {
+        type: 'datetime'
+      }
+    };
+    
+    var chart = new ApexCharts(document.querySelector("#chart"), options);
+    
+    chart.render();
+  }
+
+  public getInverters(): void{
+    this.inverterService.getInverters().subscribe(
+      (response: Inverter[]) => {
+        this.inverters = response;
+        this.renderDataForChart(this.inverters);
+        console.log(this.inverters);
+        this.renderChart(this.inverters);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    );
+  }
+
+  private renderDataForChart(inverters: Inverter[]): void{
+    const tempDatas: Inverter[] = [];
+    for (let data1 in inverters) {
+        const tempData: Inverter = inverters[data1];
+        delete tempData.id;
+        const newListData: InverterData[] = []
+        for (let data2 in tempData.data) {
+          delete tempData.data[data2].inverterDeviceId;
+          delete tempData.data[data2].id;
+          tempData.data[data2].x = tempData.data[data2].timestamp;
+          tempData.data[data2].y = tempData.data[data2].acp;
+          newListData.push(tempData.data[data2]);
+        }
+        tempDatas.push(tempData);
+    }
+    this.inverters = tempDatas;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgApexchartsModule } from 'ng-apexcharts';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { InverterService } from './inverter.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [InverterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
